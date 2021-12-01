ca8d1e82a6557f870b1becc43dd45c0ee8318cc4
Anwar Barakat <84292092+Anwar-Barakat@users.noreply.github.com>
2021-12-02 01:03:43 +0200

    some css & js on file input

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -5,7 +5,8 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <form method="post" action="{{route('admin.setting.update','test')}}" autocomplete="off" class="form-horizontal" enctype="multipart/form-data">
+                    <form method="post" action="{{ route('admin.setting.update', 'test') }}" autocomplete="off"
+                        class="form-horizontal" enctype="multipart/form-data">
                         @csrf
                         @method('put')
                         <div class="card ">
@@ -29,23 +30,20 @@
                                 @endif
                                 <div class="row mb-4">
                                     <div class="mb-3 m-auto">
-                                        <img    class="img-thumbnail"
-                                                src="{{ URL::asset('attachments/setting/'.$setting['hotel_logo']) }}"
-                                                style="box-shadow:0 0.7rem 0.6rem 0.2rem rgb(0 0 0 / 10%)">
+                                        <img class="img-thumbnail"
+                                            src="{{ URL::asset('attachments/setting/' . $setting['hotel_logo']) }}"
+                                            style="box-shadow:0 0.7rem 0.6rem 0.2rem rgb(0 0 0 / 10%)">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <label class="col-sm-2 col-form-label">{{ __('hotel name') }}</label>
                                     <div class="col-sm-7">
                                         <div class="form-group{{ $errors->has('hotel_name') ? ' has-danger' : '' }}">
-                                            <input  class="form-control{{ $errors->has('hotel_name') ? ' is-invalid' : '' }}"
-                                                    name="hotel_name"
-                                                    id="input-name"
-                                                    type="text"
-                                                    placeholder="{{ __('hotel name') }}"
-                                                    required="true"
-                                                    value="{{ $setting['hotel_name'] }}"
-                                                    aria-required="true" />
+                                            <input
+                                                class="form-control{{ $errors->has('hotel_name') ? ' is-invalid' : '' }}"
+                                                name="hotel_name" id="input-name" type="text"
+                                                placeholder="{{ __('hotel name') }}" required="true"
+                                                value="{{ $setting['hotel_name'] }}" aria-required="true" />
                                             @if ($errors->has('hotel_name'))
                                                 <span id="name-error" class="error text-danger"
                                                     for="input-name">{{ $errors->first('hotel_name') }}</span>
@@ -57,14 +55,11 @@
                                     <label class="col-sm-2 col-form-label">{{ __('hotel email') }}</label>
                                     <div class="col-sm-7">
                                         <div class="form-group{{ $errors->has('hotel_email') ? ' has-danger' : '' }}">
-                                            <input class="form-control{{ $errors->has('hotel_email') ? ' is-invalid' : '' }}"
-                                                    name="hotel_email"
-                                                    id="input-name"
-                                                    type="email"
-                                                    placeholder="{{ __('hotel email') }}"
-                                                    required="true"
-                                                    value="{{ $setting['hotel_email'] }}"
-                                                    aria-required="true" />
+                                            <input
+                                                class="form-control{{ $errors->has('hotel_email') ? ' is-invalid' : '' }}"
+                                                name="hotel_email" id="input-name" type="email"
+                                                placeholder="{{ __('hotel email') }}" required="true"
+                                                value="{{ $setting['hotel_email'] }}" aria-required="true" />
                                             @if ($errors->has('hotel_email'))
                                                 <span id="name-error" class="error text-danger"
                                                     for="input-name">{{ $errors->first('hotel_email') }}</span>
@@ -76,14 +71,11 @@
                                     <label class="col-sm-2 col-form-label">{{ __('hotel phone') }}</label>
                                     <div class="col-sm-7">
                                         <div class="form-group{{ $errors->has('hotel_phone') ? ' has-danger' : '' }}">
-                                            <input class="form-control{{ $errors->has('hotel_phone') ? ' is-invalid' : '' }}"
-                                                    name="hotel_phone"
-                                                    id="input-name"
-                                                    type="text"
-                                                    placeholder="{{ __('hotel phone') }}"
-                                                    required="true"
-                                                    value="{{ $setting['hotel_phone'] }}"
-                                                    aria-required="true" />
+                                            <input
+                                                class="form-control{{ $errors->has('hotel_phone') ? ' is-invalid' : '' }}"
+                                                name="hotel_phone" id="input-name" type="text"
+                                                placeholder="{{ __('hotel phone') }}" required="true"
+                                                value="{{ $setting['hotel_phone'] }}" aria-required="true" />
                                             @if ($errors->has('hotel_phone'))
                                                 <span id="name-error" class="error text-danger"
                                                     for="input-name">{{ $errors->first('hotel_phone') }}</span>
@@ -95,14 +87,11 @@
                                     <label class="col-sm-2 col-form-label">{{ __('hotel address') }}</label>
                                     <div class="col-sm-7">
                                         <div class="form-group{{ $errors->has('hotel_address') ? ' has-danger' : '' }}">
-                                            <input class="form-control{{ $errors->has('hotel_address') ? ' is-invalid' : '' }}"
-                                                    name="hotel_address"
-                                                    id="input-name"
-                                                    type="text"
-                                                    placeholder="{{ __('hotel address') }}"
-                                                    required="true"
-                                                    value="{{ $setting['hotel_address'] }}"
-                                                    aria-required="true" />
+                                            <input
+                                                class="form-control{{ $errors->has('hotel_address') ? ' is-invalid' : '' }}"
+                                                name="hotel_address" id="input-name" type="text"
+                                                placeholder="{{ __('hotel address') }}" required="true"
+                                                value="{{ $setting['hotel_address'] }}" aria-required="true" />
                                             @if ($errors->has('hotel_address'))
                                                 <span id="name-error" class="error text-danger"
                                                     for="input-name">{{ $errors->first('hotel_address') }}</span>
@@ -110,10 +99,16 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row">
+                                <div class="row mt-2">
                                     <label class="col-lg-2 col-form-label font-weight-semibold">Hotel logo</label>
-                                    <div class="col-lg-9">
-                                        <input name="logo" accept="image/*" type="file" class="form-control" data-show-caption="false" data-show-upload="false" data-fouc>
+                                    <div class="col-sm-7">
+                                        <div class="file-upload">
+                                            <div class="file-upload-select">
+                                                <div class="file-select-button">Choose File</div>
+                                                <div class="file-select-name">No file chosen...</div>
+                                                <input type="file" name="logo" accept="image/*" id="file-upload-input">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -127,3 +122,15 @@
         </div>
     </div>
 @endsection
+<script>
+    let fileInput = document.getElementById("file-upload-input");
+    let fileSelect = document.getElementsByClassName("file-upload-select")[0];
+    fileSelect.onclick = function() {
+        fileInput.click();
+    }
+    fileInput.onchange = function() {
+        let filename = fileInput.files[0].name;
+        let selectName = document.getElementsByClassName("file-select-name")[0];
+        selectName.innerText = filename;
+    }
+</script>
