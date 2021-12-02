4cc16ee376dec13a8154df7224399bc763ce5548
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-12-02 19:55:03 +0200

    remove duplicate 'use' of UserController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 
